945323ebf8fae30eff7fcd62b58e1d949f21a2c8
Brent Goodrick <bgoodr@gmail.com>
2014-09-20 21:27:59 -0700

    build gnome-common package needed by atk package

---
+++ packages/gnome-common/README.md
@@ -0,0 +1,7 @@
+gnome-common
+============
+
+Parent: [build-locally](../../README.md)
+
+Builds the gnome-common package. See [gnome-common home page](https://launchpad.net/gnome-common)
+
---
+++ packages/gnome-common/linux/build.bash
@@ -0,0 +1,116 @@
+#!/bin/bash
+# -*-mode: Shell-script; indent-tabs-mode: nil; sh-basic-offset: 2 -*-
+
+# Find the base directory while avoiding subtle variations in $0:
+dollar0=`which $0`; PACKAGE_DIR=$(cd $(dirname $dollar0); pwd) # NEVER export PACKAGE_DIR
+
+# Set defaults for BUILD_DIR and INSTALL_DIR environment variables and
+# utility functions such as BuildDependentPackage:
+. $PACKAGE_DIR/../../../support-files/build_platform_util.bash
+# Define perl utility functions:
+. $PACKAGE_DIR/../../../support-files/perl_util.bash
+
+usage () {
+  cat <<EOF
+USAGE: $0 ... options ...
+
+Options are:
+
+[ -builddir BUILD_DIR ]
+
+  Override the BUILD_DIR default, which is $BUILD_DIR.
+
+[ -installdir INSTALL_DIR ]
+
+  Override the INSTALL_DIR default, which is $INSTALL_DIR.
+
+EOF
+}
+
+while [ $# -gt 0 ]
+do
+  if [ "$1" = "-builddir" ]
+  then
+    BUILDDIR="$2"
+    shift
+  elif [ "$1" = "-installdir" ]
+  then
+    INSTALLDIR="$2"
+    shift
+  elif [ "$1" = "-h" ]
+  then
+    usage
+    exit 0
+  else
+    echo "Undefined parameter $1"
+    exit 1
+  fi
+  shift
+done
+
+# --------------------------------------------------------------------------------
+# Dependent packages will be installed into $INSTALL_DIR/bin so add
+# that directory to the PATH:
+# --------------------------------------------------------------------------------
+SetupBasicEnvironment
+
+# --------------------------------------------------------------------------------
+# Build required dependent packages:
+# --------------------------------------------------------------------------------
+BuildDependentPackage autoconf bin/autoconf
+BuildDependentPackage automake bin/automake
+BuildDependentPackage texinfo bin/makeinfo
+BuildDependentPackage pkg-config bin/pkg-config
+
+# --------------------------------------------------------------------------------
+# Dependent packages will be installed into $INSTALL_DIR/bin so add
+# that directory to the PATH:
+# --------------------------------------------------------------------------------
+SetupBasicEnvironment
+
+# --------------------------------------------------------------------------------
+# Create build directory structure:
+# --------------------------------------------------------------------------------
+CreateAndChdirIntoBuildDir gnome-common
+
+# --------------------------------------------------------------------------------
+# Check out the source for emacs into the build directory:
+# --------------------------------------------------------------------------------
+packageSubDir=gnome-common
+DownloadPackageFromGitRepo git://git.gnome.org/$packageSubDir $packageSubDir
+
+PrintRun cd $packageSubDir
+
+# --------------------------------------------------------------------------------
+# Configure
+# --------------------------------------------------------------------------------
+echo "Configuring ..."
+# Run autogen.sh which also runs ./configure:
+PrintRun ./autogen.sh --prefix="$INSTALL_DIR"
+
+# --------------------------------------------------------------------------------
+# Build:
+# --------------------------------------------------------------------------------
+echo "Building ..."
+PrintRun make
+
+# --------------------------------------------------------------------------------
+# Install:
+# --------------------------------------------------------------------------------
+echo "Installing ..."
+PrintRun make install
+
+# --------------------------------------------------------------------------------
+# Testing:
+# --------------------------------------------------------------------------------
+echo "Testing ..."
+
+gnomeautogenFile="$INSTALL_DIR/bin/gnome-autogen.sh"
+if [ ! -f "$gnomeautogenFile" ]
+then
+  echo "ERROR: Could not find expected file at: $gnomeautogenFile"
+  exit 1
+fi
+
+echo "Note: All installation tests passed. gnome-common was built and installed."
+exit 0
